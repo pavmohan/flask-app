0e4313aa7956260f98d493521df93a37dbc117f8
pavmohan <mohanp@oregonstate.edu>
2017-04-09 02:02:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import logging
-
 from flask import Flask
-
-
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
 
 
-@app.errorhandler(500)
-def server_error(e):
-    # Log the error and stacktrace.
-    logging.exception('An error occurred during a request.')
-    return 'An internal error occurred.', 500
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
